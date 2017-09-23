3c799b8a699e4fe5e43615102e1e06d7a1b15b13
anmol gupta <anmolgi1401@gmail.com>
2017-09-23 08:04:29 +0530

    latest code

---
+++ simplecounter/app.js
@@ -0,0 +1,43 @@
+var express=require('express');
+var socket=require('socket.io');
+var app=express();
+
+var server=app.listen(5000,function () {
+  console.log("lisiting to port no 5000");
+});
+app.use(express.static('public'));
+app.get('/',function (req,res) {
+  res.sendfile('index.html');
+});
+app.get('/view',function (req,res) {
+  res.sendfile('view.html');
+});
+
+var io =socket(server);
+var room=0;
+
+var a=0;
+io.on('connection',function (socket) {
+  room+=1;
+  console.log('connection made', room);
+  socket.on('disconnect',function(){
+    room--;
+    console.log('connection made',room);
+  });
+
+  socket.on('count',function (val) {
+    a=val;
+
+    io.sockets.emit('count',a);
+
+  });
+  socket.on('reset',function (reset) {
+    io.sockets.emit('count',reset);
+
+  });
+  socket.on('decrement',function (val) {
+    io.sockets.emit('count',val);
+
+  });
+
+});
---
+++ simplecounter/public/chat.js
@@ -0,0 +1,40 @@
+var socket =io.connect("http://localhost:5000");
+var output =document.getElementById('output'),
+    btn=document.getElementById('send'),
+    btn2=document.getElementById('reset'),
+    btn3=document.getElementById('decrement');
+
+var val=0;
+//event listener
+btn.addEventListener('click',function () {
+    val+=1;
+    console.log(val);
+    socket.emit('count',val);
+});
+btn2.addEventListener('click',function(){
+  var reset=0;
+  socket.emit('reset',reset);
+});
+
+btn3.addEventListener('click',function(){
+  val-=1;
+  socket.emit('decrement',val);
+});
+
+
+
+ socket.on('count',function (a) {
+   val = a;
+   output.innerHTML='<p><strong>'+ val +  '</strong></p>';
+
+ });
+ socket.on('reset',function (reset) {
+
+   output.innerHTML='<p><strong>'+ reset +  '</strong></p>';
+
+ });
+ socket.on('decrement',function (a) {
+   val = a;
+   output.innerHTML='<p><strong>'+ val +  '</strong></p>';
+
+ });
